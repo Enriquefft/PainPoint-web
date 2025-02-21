7ace0a0534b71a40b73b9d686ab9c9ce3f0062ea
Enriquefft <enriquefft2001@gmail.com>
2025-02-21 13:50:07 -0500

    style: format

--- src/app/WhatsappButton.tsx
+++ src/app/WhatsappButton.tsx
@@ -1,41 +1,38 @@
-'use client'
+"use client";
 
-import Image from "next/image"
 import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
+	AlertDialog,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import Image from "next/image";
 
 export default function WhatsappButton() {
-                                    // <a href="https://wa.me/+18573494519" rel="noreferrer" target="_blank">
-    // </a>
-                                    return (
-
-<AlertDialog>
-  <AlertDialogTrigger>
-
-
-                                        <Image
-                                            src="/whatsapp_button/ChatOnWhatsAppButton/WhatsAppButtonGreenLarge.svg"
-                                            alt="WhatsApp icon"
-                    className="cursor-pointer translate-5"
-                                            height="40"
-                                            width="200"
-                                        />
-  </AlertDialogTrigger>
-  <AlertDialogContent>
-    <AlertDialogHeader>
-      <AlertDialogTitle>In progress</AlertDialogTitle>
-      <AlertDialogDescription>
-      We are still waiting for whatsapp to approve the phone numbers name.
-      Meta is painfully slow, fml
-      </AlertDialogDescription>
-    </AlertDialogHeader>
-  </AlertDialogContent>
-</AlertDialog>
-);
+	// <a href="https://wa.me/+18573494519" rel="noreferrer" target="_blank">
+	// </a>
+	return (
+		<AlertDialog>
+			<AlertDialogTrigger>
+				<Image
+					src="/whatsapp_button/ChatOnWhatsAppButton/WhatsAppButtonGreenLarge.svg"
+					alt="WhatsApp icon"
+					className="cursor-pointer translate-5"
+					height="40"
+					width="200"
+				/>
+			</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>In progress</AlertDialogTitle>
+					<AlertDialogDescription>
+						We are still waiting for whatsapp to approve the phone numbers name.
+						Meta is painfully slow, fml
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
 }
