a039539a62f871b1d85ec89208fc328d5234df4f
Enriquefft <enriquefft2001@gmail.com>
2025-02-24 15:06:40 -0500

    feat: added images

--- src/metadata.ts
+++ src/metadata.ts
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
 	metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
 	title: {
-		template: "%s | Nextjs template",
-		default: "Nextjs template",
+		template: "%s | PainPoint",
+		default: "PainPoint",
 	},
-	description: "Nextjs template",
-	keywords: ["Nextjs", "fullstack", "templates"],
+	description: "App for first-time founders to practice and improve their user interviewing skills.",
+	keywords: ["Startup", "Founder", "AI", "the mom test", "customer discovery"],
 	authors: [
 		{
 			name: "Enrique Flores",
