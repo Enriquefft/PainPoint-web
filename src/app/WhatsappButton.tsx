aff8449a8776f2068db4b74d96400e5d401a444f
Enriquefft <enriquefft2001@gmail.com>
2025-02-26 16:17:07 -0500

    feat: added wsp number

--- src/app/WhatsappButton.tsx
+++ src/app/WhatsappButton.tsx
@@ -1,21 +1,9 @@
 "use client";
 
-import {
-	AlertDialog,
-	AlertDialogContent,
-	AlertDialogDescription,
-	AlertDialogHeader,
-	AlertDialogTitle,
-	AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 import Image from "next/image";
 
 export default function WhatsappButton() {
-	// <a href="https://wa.me/+18573494519" rel="noreferrer" target="_blank">
-	// </a>
-	return (
-		<AlertDialog>
-			<AlertDialogTrigger>
+	return <a href="https://wa.me/+18573494519" rel="noreferrer" target="_blank">
 				<Image
 					src="/whatsapp_button/ChatOnWhatsAppButton/WhatsAppButtonGreenLarge.svg"
 					alt="WhatsApp icon"
@@ -23,16 +11,5 @@ export default function WhatsappButton() {
 					height="40"
 					width="200"
 				/>
-			</AlertDialogTrigger>
-			<AlertDialogContent>
-				<AlertDialogHeader>
-					<AlertDialogTitle>In progress</AlertDialogTitle>
-					<AlertDialogDescription>
-						We are still waiting for whatsapp to approve the phone numbers name.
-						Meta is painfully slow, fml
-					</AlertDialogDescription>
-				</AlertDialogHeader>
-			</AlertDialogContent>
-		</AlertDialog>
-	);
+	</a>
 }
