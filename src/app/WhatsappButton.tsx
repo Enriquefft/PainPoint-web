c68f04f81dcb02622b521d413c59695e8bb9b9f5
Enriquefft <enriquefft2001@gmail.com>
2025-02-26 16:17:15 -0500

    style: format

--- src/app/WhatsappButton.tsx
+++ src/app/WhatsappButton.tsx
@@ -3,13 +3,15 @@
 import Image from "next/image";
 
 export default function WhatsappButton() {
-	return <a href="https://wa.me/+18573494519" rel="noreferrer" target="_blank">
-				<Image
-					src="/whatsapp_button/ChatOnWhatsAppButton/WhatsAppButtonGreenLarge.svg"
-					alt="WhatsApp icon"
-					className="cursor-pointer"
-					height="40"
-					width="200"
-				/>
-	</a>
+	return (
+		<a href="https://wa.me/+18573494519" rel="noreferrer" target="_blank">
+			<Image
+				src="/whatsapp_button/ChatOnWhatsAppButton/WhatsAppButtonGreenLarge.svg"
+				alt="WhatsApp icon"
+				className="cursor-pointer"
+				height="40"
+				width="200"
+			/>
+		</a>
+	);
 }
